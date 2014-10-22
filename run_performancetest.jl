61c59fcaeda2f5ff5d87bae907e52990ad6e3293
Rasmus Brandt <rasmusb85@gmail.com>
2014-10-22 14:15:39 +0200

    Added performance test.

---
+++ run_performancetest.jl
@@ -0,0 +1,40 @@
+#!/usr/bin/env julia
+
+##########################################################################
+# run_performancetest.jl
+#
+# Compares runtimes of different methods.
+##########################################################################
+
+include("src/IARegularizedWMMSEEval.jl")
+using IARegularizedWMMSEEval, CoordinatedPrecoding
+
+##########################################################################
+# General settings
+srand(8071232234)
+start_time = strftime("%Y%m%dT%H%M%S", time())
+
+##########################################################################
+# Performance test
+simulation_params = {
+    "I" => 3, "Kc" => 1, "N" => 2, "M" => 2,
+    "P_dBm" => 20.,
+    "d" => 1,
+    "Ntest" => 100,
+    "precoding_methods" => [
+        Shi2011_WMMSE,
+        Gomadam2008_MaxSINR,
+        IARegularizedWMMSE,
+        Eigenprecoding
+    ]
+}
+precoding_settings = {
+    "stop_crit" => 20,
+}
+network =
+    setup_interfering_broadcast_channel(simulation_params["I"],
+        simulation_params["Kc"], simulation_params["N"], simulation_params["M"],
+        transmit_power=10^(simulation_params["P_dBm"]/10),
+        no_streams=simulation_params["d"])
+
+perform_performancetest(network, simulation_params, precoding_settings)

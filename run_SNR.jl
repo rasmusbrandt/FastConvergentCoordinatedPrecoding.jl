51cf411ea27e596a77ea9ac3512960dbd75dbee5
Rasmus Brandt <rasmusb85@gmail.com>
2014-12-04 15:46:04 +0100

    We can now handle IBCs.

--- run_SNR.jl
+++ run_SNR.jl
@@ -54,42 +54,42 @@ save("SNR_$(simulation_params["name"]).jld",
      "precoding_settings", clean_precoding_settings_for_jld(precoding_settings),
      "results", results)
 
-# ##########################################################################
-# # Interfering broadcast channel channel
-# simulation_params = [
-#     "name" => "$(start_time)-ibc",
-#     "I" => 3, "Kc" => 2, "N" => 2, "M" => 4,
-#     "Ps_dBm" => 0:3:40,
-#     "d" => 1,
-#     "Ndrops" => 10, "Nsim" => 1,
-#     "precoding_methods" => [
-#         LogDetHeuristic,
-#         # NuclearNormHeuristic,
+##########################################################################
+# Interfering broadcast channel channel
+simulation_params = [
+    "name" => "$(start_time)-ibc",
+    "I" => 3, "Kc" => 2, "N" => 2, "M" => 4,
+    "Ps_dBm" => 0:3:40,
+    "d" => 1,
+    "Ndrops" => 10, "Nsim" => 1,
+    "precoding_methods" => [
+        LogDetHeuristic,
+        # NuclearNormHeuristic,
 
-#         Shi2011_WMMSE,
-#         Gomadam2008_MaxSINR,
-#         Eigenprecoding
-#     ]
-# ]
-# precoding_settings["user_priorities"] = ones(simulation_params["I"]*simulation_params["Kc"])
-# network =
-#     setup_interfering_broadcast_channel(simulation_params["I"],
-#         simulation_params["Kc"], simulation_params["N"], simulation_params["M"],
-#         no_streams=simulation_params["d"])
+        Shi2011_WMMSE,
+        Gomadam2008_MaxSINR,
+        Eigenprecoding
+    ]
+]
+precoding_settings["user_priorities"] = ones(simulation_params["I"]*simulation_params["Kc"])
+network =
+    setup_interfering_broadcast_channel(simulation_params["I"],
+        simulation_params["Kc"], simulation_params["N"], simulation_params["M"],
+        no_streams=simulation_params["d"])
 
-# results = simulate_SNR(network, simulation_params, precoding_settings)
+results = simulate_SNR(network, simulation_params, precoding_settings)
 
-# println("-- Saving $(simulation_params["name"]) results")
-# save("SNR_$(simulation_params["name"]).jld",
-#      "simulation_params", clean_simulation_params_for_jld(simulation_params),
-#      "precoding_settings", clean_precoding_settings_for_jld(precoding_settings),
-#      "results", results)
+println("-- Saving $(simulation_params["name"]) results")
+save("SNR_$(simulation_params["name"]).jld",
+     "simulation_params", clean_simulation_params_for_jld(simulation_params),
+     "precoding_settings", clean_precoding_settings_for_jld(precoding_settings),
+     "results", results)
 
 ##########################################################################
 # Triangular3Site network
 simulation_params = [
     "name" => "$(start_time)-triangular3site",
-    "I" => 3, "Kc" => 2, "N" => 2, "M" => 2,
+    "I" => 3, "Kc" => 2, "N" => 2, "M" => 4,
     "P_dBm" => 18.2,
     "d" => 1,
     "Ndrops" => 10, "Nsim" => 1,

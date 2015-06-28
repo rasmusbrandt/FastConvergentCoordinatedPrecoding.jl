5acff701af76a11da6eff803d08bfd6ba5296e5d
Rasmus Brandt <rasmusb85@gmail.com>
2015-06-28 13:35:13 +0200

    Didn't work on delphi; try fewer Ndrops... :(

--- batch/conference_publication/run_rho-base.jl
+++ batch/conference_publication/run_rho-base.jl
@@ -16,7 +16,7 @@ simulation_params = [
     "I" => 6, "Kc" => 1, "N" => 2, "M" => 3,
     "P_dBm" => 30.,
     "d" => 1,
-    "Ndrops" => 100, "Nsim" => 1,
+    "Ndrops" => 50, "Nsim" => 1,
     "precoding_methods" => [
         LogDetHeuristic,
 

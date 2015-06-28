3562306ce75fb9795e239c27d67f65bcf2a9ab36
Rasmus Brandt <rasmusb85@gmail.com>
2015-06-28 14:07:05 +0200

    Switch back to 20 rho granularity.

--- batch/conference_publication/run_rho-base.jl
+++ batch/conference_publication/run_rho-base.jl
@@ -36,7 +36,7 @@ simulation_params = [
 
         "delta" => 1.,
     ],
-    "independent_variable" => ((n, v) -> set_aux_precoding_param!(n, v, "rho"), logspace(-1, 2, 100)),
+    "independent_variable" => ((n, v) -> set_aux_precoding_param!(n, v, "rho"), logspace(-1, 2, 20)),
     "aux_independent_variables" => [
         ((n, v) -> set_aux_precoding_param!(n, v, "turbo_iters"), [1, 2, 4]),
     ]

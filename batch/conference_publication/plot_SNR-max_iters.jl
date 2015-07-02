6048028e4f419a1ee94cf24d706dccae3f72a3ef
Rasmus Brandt <rasmusb85@gmail.com>
2015-07-02 16:23:52 +0200

    Nicer plots.

--- batch/conference_publication/plot_SNR-max_iters.jl
+++ batch/conference_publication/plot_SNR-max_iters.jl
@@ -17,14 +17,14 @@ data = load("SNR-max_iters-merged.jld")
 
 ##########################################################################
 # Build figure
-PyPlot.rc("lines", linewidth=1., markersize=6)
+PyPlot.rc("lines", linewidth=1., markersize=3, markeredgewidth=0.5)
 PyPlot.rc("font", size=8, family="serif", serif="Computer Modern Sans Serif")
 PyPlot.rc("text", usetex=true)
 PyPlot.rc("text.latex", preamble="\\usepackage{amsmath}")
 PyPlot.rc("axes", linewidth=0.5, labelsize=8)
 PyPlot.rc("xtick", labelsize=8)
 PyPlot.rc("ytick", labelsize=8)
-PyPlot.rc("legend", fancybox=true, fontsize=5)
+PyPlot.rc("legend", fancybox=true, fontsize=6)
 PyPlot.rc("figure", figsize=(3.50,2.16), dpi=125)
 
 fig = PyPlot.figure()
@@ -32,26 +32,30 @@ ax = fig[:add_axes]((0.11,0.15,0.95-0.11,0.95-0.15))
 
 xvals = data["simulation_params"]["independent_variable"][2]
 
-ax[:plot](xvals, data["results_mean"]["LogDetHeuristic"]["logdet_rates"][:,3], color="g", linestyle="-", label=L"FastDCP ($L_\text{OTA} = 3$)")
-ax[:plot](xvals, data["results_mean"]["LogDetHeuristic"]["logdet_rates"][:,2], color="g", linestyle="--", label=L"FastDCP ($L_\text{OTA} = 2$)")
-ax[:plot](xvals, data["results_mean"]["LogDetHeuristic"]["logdet_rates"][:,1], color="g", linestyle=":", label=L"FastDCP ($L_\text{OTA} = 1$)")
-ax[:plot](xvals, data["results_mean"]["Shi2011_WMMSE"]["logdet_rates"][:,3], color="b", linestyle="-", label=L"WMMSE ($L_\text{OTA} = 3$)")
-ax[:plot](xvals, data["results_mean"]["Shi2011_WMMSE"]["logdet_rates"][:,2], color="b", linestyle="--", label=L"WMMSE ($L_\text{OTA} = 2$)")
-ax[:plot](xvals, data["results_mean"]["Shi2011_WMMSE"]["logdet_rates"][:,1], color="b", linestyle=":", label=L"WMMSE ($L_\text{OTA} = 1$)")
-ax[:plot](xvals, data["results_mean"]["Du2013_ReweightedRCRM"]["logdet_rates"][:,3], color="r", linestyle="-", label=L"Reweighted RCRM ($L_\text{OTA} = 3$)")
-ax[:plot](xvals, data["results_mean"]["Du2013_ReweightedRCRM"]["logdet_rates"][:,2], color="r", linestyle="--", label=L"Reweighted RCRM ($L_\text{OTA} = 2$)")
-ax[:plot](xvals, data["results_mean"]["Du2013_ReweightedRCRM"]["logdet_rates"][:,1], color="r", linestyle=":", label=L"Reweighted RCRM ($L_\text{OTA} = 1$)")
+# Markers
+l1 = ax[:plot](xvals, data["results_mean"]["LogDetHeuristic"]["logdet_rates"][:,3], color="g", markeredgecolor="g", marker="o", linestyle="-", label=L"FastDCP ($L_\text{OTA} = 3$)")
+l2 = ax[:plot](xvals, data["results_mean"]["LogDetHeuristic"]["logdet_rates"][:,2], color="g", markeredgecolor="g", marker="o", linestyle="--", label=L"FastDCP ($L_\text{OTA} = 2$)")
+l3 = ax[:plot](xvals, data["results_mean"]["LogDetHeuristic"]["logdet_rates"][:,1], color="g", markeredgecolor="g", marker="o", linestyle=":", label=L"FastDCP ($L_\text{OTA} = 1$)")
+l4 = ax[:plot](xvals, data["results_mean"]["Shi2011_WMMSE"]["logdet_rates"][:,3], color="b", markeredgecolor="b", marker="v", linestyle="-", label=L"WMMSE ($L_\text{OTA} = 3$)")
+l5 = ax[:plot](xvals, data["results_mean"]["Shi2011_WMMSE"]["logdet_rates"][:,2], color="b", markeredgecolor="b", marker="v", linestyle="--", label=L"WMMSE ($L_\text{OTA} = 2$)")
+l6 = ax[:plot](xvals, data["results_mean"]["Shi2011_WMMSE"]["logdet_rates"][:,1], color="b", markeredgecolor="b", marker="v", linestyle=":", label=L"WMMSE ($L_\text{OTA} = 1$)")
+l7 = ax[:plot](xvals, data["results_mean"]["Du2013_ReweightedRCRM"]["logdet_rates"][:,3], color="r", markeredgecolor="r", marker="^", linestyle="-", label=L"Reweighted RCRM ($L_\text{OTA} = 3$)")
+l8 = ax[:plot](xvals, data["results_mean"]["Du2013_ReweightedRCRM"]["logdet_rates"][:,2], color="r", markeredgecolor="r", marker="^", linestyle="--", label=L"Reweighted RCRM ($L_\text{OTA} = 2$)")
+l9 = ax[:plot](xvals, data["results_mean"]["Du2013_ReweightedRCRM"]["logdet_rates"][:,1], color="r", markeredgecolor="r", marker="^", linestyle=":", label=L"Reweighted RCRM ($L_\text{OTA} = 1$)")
 
 ax[:set_ylim](0, 25)
 
 ax[:set_xlabel]("Signal-to-noise ratio [dB]")
 ax[:set_ylabel]("Sum rate [bits/s/Hz]")
 
-legend = ax[:legend](loc="upper left")
-# legend_lines = legend[:get_lines]()
-legend_frame = legend[:get_frame]()
-# PyPlot.setp(legend_lines, linewidth=0.5)
-PyPlot.setp(legend_frame, linewidth=0.5)
+legend1 = PyPlot.legend(handles = [l1, l2, l3, l4, l5, l6], loc="upper left")
+legend1_frame = legend1[:get_frame]()
+PyPlot.setp(legend1_frame, linewidth=0.5)
+ax[:add_artist](legend1)
+
+legend2 = ax[:legend](handles = [l7, l8, l9], loc="lower right")
+legend2_frame = legend2[:get_frame]()
+PyPlot.setp(legend2_frame, linewidth=0.5)
 
 ##########################################################################
 # Write file
--- batch/conference_publication/plot_rho.jl
+++ batch/conference_publication/plot_rho.jl
@@ -17,7 +17,7 @@ data = load("rho-merged.jld")
 
 ##########################################################################
 # Build figure
-PyPlot.rc("lines", linewidth=1., markersize=6)
+PyPlot.rc("lines", linewidth=1., markersize=3, markeredgewidth=0.5)
 PyPlot.rc("font", size=8, family="serif", serif="Computer Modern Sans Serif")
 PyPlot.rc("text", usetex=true)
 PyPlot.rc("text.latex", preamble="\\usepackage{amsmath}")
@@ -32,13 +32,23 @@ ax = fig[:add_axes]((0.11,0.15,0.95-0.11,0.95-0.15))
 
 xvals = data["simulation_params"]["independent_variable"][2]
 
-ax[:plot](xvals, data["results_mean"]["LogDetHeuristic"]["logdet_rates"][:,3], color="g", linestyle="-", label=L"FastDCP ($L_\text{local} = 4$)")
-ax[:plot](xvals, data["results_mean"]["LogDetHeuristic"]["logdet_rates"][:,2], color="g", linestyle="--", label=L"FastDCP ($L_\text{local} = 2$)")
-ax[:plot](xvals, data["results_mean"]["LogDetHeuristic"]["logdet_rates"][:,1], color="g", linestyle=":", label=L"FastDCP ($L_\text{local} = 1$)")
-# ax[:plot](xvals, data["results_mean"]["Eigenprecoding"]["intercell_tdma_logdet_rates"][:,1], color="c", linestyle="-", label="TDMA")
-ax[:plot](xvals, data["results_mean"]["Shi2011_WMMSE"]["logdet_rates"][:,1], color="b", linestyle="-", label="WMMSE [7]")
-ax[:plot](xvals, data["results_mean"]["Du2013_ReweightedRCRM"]["logdet_rates"][:,1], color="r", linestyle="-", label="Reweighted RCRM [11]")
-# ax[:plot](xvals, data["results_mean"]["Eigenprecoding"]["uncoord_logdet_rates"][:,1], color="k", linestyle="-", label="Uncoordinated transmission")
+# Lines
+ax[:plot](xvals, data["results_mean"]["LogDetHeuristic"]["logdet_rates"][:,3], color="g", linestyle="-")
+ax[:plot](xvals, data["results_mean"]["LogDetHeuristic"]["logdet_rates"][:,2], color="g", linestyle="--")
+ax[:plot](xvals, data["results_mean"]["LogDetHeuristic"]["logdet_rates"][:,1], color="g", linestyle=":")
+# ax[:plot](xvals, data["results_mean"]["Eigenprecoding"]["intercell_tdma_logdet_rates"][:,1], color="c", linestyle="-")
+ax[:plot](xvals, data["results_mean"]["Shi2011_WMMSE"]["logdet_rates"][:,1], color="b", linestyle="-")
+ax[:plot](xvals, data["results_mean"]["Du2013_ReweightedRCRM"]["logdet_rates"][:,1], color="r", linestyle="-")
+# ax[:plot](xvals, data["results_mean"]["Eigenprecoding"]["uncoord_logdet_rates"][:,1], color="k", linestyle="-")
+
+# Markers
+ax[:plot](xvals[1:3:end], data["results_mean"]["LogDetHeuristic"]["logdet_rates"][:,3][1:3:end], color="g", marker="o", markeredgecolor="g", linestyle="-", label=L"FastDCP ($L_\text{local} = 4$)")
+ax[:plot](xvals[1:3:end], data["results_mean"]["LogDetHeuristic"]["logdet_rates"][:,2][1:3:end], color="g", marker="o", markeredgecolor="g", linestyle="--", label=L"FastDCP ($L_\text{local} = 2$)")
+ax[:plot](xvals[1:3:end], data["results_mean"]["LogDetHeuristic"]["logdet_rates"][:,1][1:3:end], color="g", marker="o", markeredgecolor="g", linestyle=":", label=L"FastDCP ($L_\text{local} = 1$)")
+# ax[:plot](xvals[1:3:end], data["results_mean"]["Eigenprecoding"]["intercell_tdma_logdet_rates"][:,1][1:3:end], color="c", marker="x", markeredgecolor="c", linestyle="-", label="TDMA")
+ax[:plot](xvals[1:3:end], data["results_mean"]["Shi2011_WMMSE"]["logdet_rates"][:,1][1:3:end], color="b", marker="v", markeredgecolor="b", linestyle="-", label="WMMSE [7]")
+ax[:plot](xvals[1:3:end], data["results_mean"]["Du2013_ReweightedRCRM"]["logdet_rates"][:,1][1:3:end], color="r", marker="^", markeredgecolor="r", linestyle="-", label="Reweighted RCRM [11]")
+# ax[:plot](xvals[1:3:end], data["results_mean"]["Eigenprecoding"]["uncoord_logdet_rates"][:,1][1:3:end], color="k", marker=".", markeredgecolor="k", linestyle="-", label="Uncoordinated transmission")
 
 ax[:set_xscale]("log")
 ax[:set_ylim](0, 26)
